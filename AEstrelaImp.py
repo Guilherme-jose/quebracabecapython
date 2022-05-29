cb41db71d6de73b91cb86cd0d14a9604d5f8d7c3
Guilherme-jose <guijose.bitencourt1999@gmail.com>
2022-05-29 14:36:02 -0300

    initial commit

---
+++ AEstrelaImp.py
@@ -0,0 +1,64 @@
+# Nome: Guilherme José Bitencourt Lopes
+# Matricula: 95660
+
+from numpy import Infinity
+from Posicao import Posicao
+from AEstrela import AEstrela
+from QuebraCabeca import QuebraCabeca
+from QuebraCabecaImp import QuebraCabecaImp
+
+
+class AEstrelaImp(AEstrela):
+    def getSolucao(self, qc):
+        qCopy = QuebraCabecaImp() 
+        qCopy.setTab(qc.getTab())
+        start = qCopy.hashCode()
+        startPos = qCopy.getTab()
+        openStates = [start]
+        g = {start : 0}
+
+        f = {start : qCopy.getValor()}
+
+        hash = {start : startPos}
+
+        map = {}
+
+        while len(openStates) > 0:
+            min = Infinity
+            current = 0
+            for i in openStates:
+                if f[i] < min:
+                    min = f[i]
+                    current = i
+
+
+            qCopy.setTab(hash[current])
+            if qCopy.isOrdenado():
+                path = [Posicao(qCopy.getPosVazio().getLinha(), qCopy.getPosVazio().getColuna())]
+                while current in map:
+                    current = map[current]
+                    qCopy.setTab(hash[current])
+                    #print(qCopy.getTab()[0])
+                    #print(qCopy.getTab()[1])
+                    #print(qCopy.getTab()[2])
+                    #print(" ")
+                    path.insert(0, Posicao(qCopy.getPosVazio().getLinha(), qCopy.getPosVazio().getColuna()))
+                return path
+            openStates.remove(current)
+            for m in qCopy.getMovePossiveis():
+                qCopy.move(qCopy.getPosVazio().getLinha(),qCopy.getPosVazio().getColuna(),m.getLinha(), m.getColuna())
+                nextState = qCopy.hashCode()
+                score = g[current] + 1 #todos os movimentos tem peso 1 neste quebra cabeça
+                if nextState not in g:
+                     g[nextState] = Infinity
+                if (score < g[nextState]):
+                    map[nextState] = current
+                    g[nextState] = score
+                    f[nextState] = score + qCopy.getValor()
+                    hash[nextState] = qCopy.getTab()
+                    if qCopy.getTab() not in openStates:
+                        openStates.append(qCopy.hashCode())
+                    
+                    #print(score)
+                qCopy.setTab(hash[current])
+        return []
